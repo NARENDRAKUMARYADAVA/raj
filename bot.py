6f854b5c53969351c51f85a3e429d09064159b81
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-07 20:08:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,5 +7,5 @@ app = Client('765108996:AAGYA2lsT6yw1q5SEx1PXesPWYdwb8RBivc')
 
 @app.on_message(Filters.chat('icc_cricket'))
 def forawrd(client, message):
-    client.forward_messages('indianLine11', 'icc_cricket', [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
+    client.forward_messages('IndianLine11', 'icc_cricket', [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
 app.run()
