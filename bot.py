ae433b0f193de6641b6ac7bfe2c188d42932ab5d
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-07 19:47:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from pyrogram import Client, Filters
 
 app = Client('765108996:AAGYA2lsT6yw1q5SEx1PXesPWYdwb8RBivc')
 
-@app.on_message(Filters.chat('https://t.me/joinchat/AAAAAENZH2KQhHhHrUvz6g'))
+@app.on_message(Filters.chat('icc_cricket'))
 def forawrd(client, message):
-    client.forward_messages('https://t.me/joinchat/AAAAAFAqabnU15MWqOSbig', 'https://t.me/joinchat/AAAAAENZH2KQhHhHrUvz6g', [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
+    client.forward_messages('indianLine11', 'icc_cricket', [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
 app.run()
