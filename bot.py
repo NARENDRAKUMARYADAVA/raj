81d4e88945d5cdc94728610dcb743b8638cc53da
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-07 17:27:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,5 +7,5 @@ app = Client('765108996:AAGYA2lsT6yw1q5SEx1PXesPWYdwb8RBivc')
 
 @app.on_message(Filters.chat('cfamovies1'))
 def forawrd(client, message):
-    client.forward_messages('icc_cricket', 'cfamovies1', 'as_copy(bool, True)' , [message.message_id])
+    client.forward_messages('icc_cricket', 'cfamovies1', [message.message_id], bool = None , bool = True , bool = True )
 app.run()
