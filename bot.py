2b3994f042fc4b9f3f04ce75484fa1de39ff4234
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-07 19:38:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from pyrogram import Client, Filters
 
 app = Client('765108996:AAGYA2lsT6yw1q5SEx1PXesPWYdwb8RBivc')
 
-@app.on_message(Filters.chat('cfamovies1'))
+@app.on_message(Filters.chat('https://t.me/joinchat/AAAAAENZH2KQhHhHrUvz6g'))
 def forawrd(client, message):
     client.forward_messages('https://t.me/joinchat/AAAAAFAqabnU15MWqOSbig', 'https://t.me/joinchat/AAAAAENZH2KQhHhHrUvz6g', [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
 app.run()
