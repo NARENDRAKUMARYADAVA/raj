dc6a5ab778105d3db23ceb8d92d2baff7d2accf5
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-07 06:49:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from pyrogram import Client, Filters
 
 app = Client('account')
 
-@app.on_message(Filters.chat('ferrariline1'))
+@app.on_message(Filters.chat('cfamovies1'))
 def forawrd(client, message):
-    client.forward_messages('icc_cricket', 'ferrariline1', [message.message_id])
+    client.forward_messages('icc_cricket', 'cfamovies1', [message.message_id])
 app.run()
