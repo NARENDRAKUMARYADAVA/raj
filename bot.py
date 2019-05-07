53be3b53bef851b43a7707c0c5971b19a7e62eae
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-07 07:46:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,5 +7,5 @@ app = Client('765108996:AAGYA2lsT6yw1q5SEx1PXesPWYdwb8RBivc')
 
 @app.on_message(Filters.chat('cfamovies1'))
 def forawrd(client, message):
-    client.send_messages('icc_cricket', 'cfamovies1', [message.message_id])
+    client.sendMessages('icc_cricket', 'cfamovies1', [message.message_id])
 app.run()
