0a92de88c0b9ee8eb876efb0aa69b4ce599c32c5
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-09 18:16:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,13 +5,14 @@ from pyrogram import Client, Filters
 
 app = Client('765108996:AAGYA2lsT6yw1q5SEx1PXesPWYdwb8RBivc')
 
-
+line = '1'
 
 
 @app.on_message(Filters.chat(-1001309160459) & Filters.text & ~ Filters.regex('👇'))
 def forawrd(client, message):
-    client.forward_messages(-1001344956857, -1001309160459, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
-    client.forward_messages(-1001356076506, -1001309160459, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
+   if line == '1' :
+      client.forward_messages(-1001344956857, -1001309160459, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
+      client.forward_messages(-1001356076506, -1001309160459, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
     
 
 app.run()
