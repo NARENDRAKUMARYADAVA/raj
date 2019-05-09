d3973304fed9bc326b980c1149517fb463f0e094
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-09 17:47:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ app = Client('765108996:AAGYA2lsT6yw1q5SEx1PXesPWYdwb8RBivc')
 
 hii ='hii','dekho','winner', 'fix', '😱', '😳', 'fixer', '👆', '👇', 'match', 'pass', 'sab', 'chase', 'defend', 'hai', 'karvana', 'link',' loss', 'audio', 'varna',' pura', 'puri',' open', 'paid', 'contact',' baazigar', 'market',' load', 'whatsapp','Baazigar','https://','Https://'
 
-@app.on_message(Filters.chat(-1001309160459) & Filters.text & ~ Filters.regex(hii))
+@app.on_message(Filters.chat(-1001309160459) & Filters.text & ~ Filters.regex('👆','👇'))
 def forawrd(client, message):
     client.forward_messages(-1001344956857, -1001309160459, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
     
