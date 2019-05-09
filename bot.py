29f968db39deb772baefc7c4ad7821da538edaf3
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-09 15:38:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,9 +5,9 @@ from pyrogram import Client, Filters
 
 app = Client('765108996:AAGYA2lsT6yw1q5SEx1PXesPWYdwb8RBivc')
 
-@app.on_message(Filters.chat('ferrariline1'))
+@app.on_message(Filters.chat('-1001309160459'))
 def forawrd(client, message):
-    client.forward_messages('-1001356076506', 'ferrariline1', [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
+    client.forward_messages('-1001356076506', '-1001309160459', [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
     edit_message_text('zearn', '2344', [message] )
 
 app.run()
