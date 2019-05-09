cf7258b6e90acaccc46f98b9e5834d8ca7391b7d
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-09 17:55:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ app = Client('765108996:AAGYA2lsT6yw1q5SEx1PXesPWYdwb8RBivc')
 
 
 
-@app.on_message(Filters.chat(-1001309160459) & Filters.text & ~ Filters.regex('👆','👇'))
+@app.on_message(Filters.chat(-1001309160459) & Filters.text & ~ Filters.regex(['👆','👇']))
 def forawrd(client, message):
     client.forward_messages(-1001344956857, -1001309160459, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
     
