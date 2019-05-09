8db09ba838698164b3d14aebd2cc238f96781a67
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-09 15:29:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ app = Client('765108996:AAGYA2lsT6yw1q5SEx1PXesPWYdwb8RBivc')
 @app.on_message(Filters.chat('ferrariline1'))
 def forawrd(client, message):
     client.forward_messages('zearn', 'ferrariline1', [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
-    client.edit_message_text('zearn', '2344', [message] )
+    edit_message_text('zearn', '2344', [message] )
 
 app.run()
 
