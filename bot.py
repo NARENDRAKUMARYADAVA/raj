b61182629b92fdcc4246d5268dc28cd2288c33e2
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-09 17:51:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from pyrogram import Client, Filters
 app = Client('765108996:AAGYA2lsT6yw1q5SEx1PXesPWYdwb8RBivc')
 
 
-hii ='hii','dekho','winner', 'fix', '😱', '😳', 'fixer', '👆', '👇', 'match', 'pass', 'sab', 'chase', 'defend', 'hai', 'karvana', 'link',' loss', 'audio', 'varna',' pura', 'puri',' open', 'paid', 'contact',' baazigar', 'market',' load', 'whatsapp','Baazigar','https://','Https://'
+
 
 @app.on_message(Filters.chat(-1001309160459) & Filters.text & ~ Filters.regex('👆','👇'))
 def forawrd(client, message):
