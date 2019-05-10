312492081780b14011d2d3dee5104b45771ccd2f
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-10 10:48:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ def main(client, message) :
 @app.on_message(Filters.command('zeets'))
 def main(client, message) :
     file = open("text.txt" , "w")
-    file.write(sez)
+    file.write("sez")
     file.close()
 
 
