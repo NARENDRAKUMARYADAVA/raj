bfc80caf78990f799aeac624ee2e565c86faf840
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-10 10:21:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ string = 'xcx'
 
 @app.on_message(Filters.chat(-1001309160459) & Filters.text & ~ Filters.regex('👇'))
 def forawrd(client, message):
-  if string = 'xcx' 
+  if string == 'xcx' 
       client.forward_messages(-1001344956857, -1001309160459, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
       client.forward_messages(-1001356076506, -1001309160459, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
 
