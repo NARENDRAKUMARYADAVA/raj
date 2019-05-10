12168a4dd1ecbf1360da3dce9fc103076a8f75d8
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-10 08:23:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,6 +41,17 @@ def ran(client , message ):
 def ran(client , message ):
       string.replace('j','m')
       message.reply(string)
+      users_ref = ref.child('users')
+      users_ref.set({
+    'alanisawesome': {
+        'date_of_birth': 'June 23, 1912',
+        'full_name': 'Alan Turing'
+    },
+    'gracehop': {
+        'date_of_birth': 'December 9, 1906',
+        'full_name': 'Grace Hopper'
+    }
+})
 
 app.run()
 
