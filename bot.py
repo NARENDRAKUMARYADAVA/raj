443a45b629f6f64def7ad3cb512c6ba2dbb063e8
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-10 09:45:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ string = 'j m t'
 
 @app.on_message(Filters.chat(-1001309160459) & Filters.text & ~ Filters.regex('👇'))
 def forawrd(client, message):
-   if string == 'j m' :
+   if string == 'j m t' :
       client.forward_messages(-1001344956857, -1001309160459, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
       client.forward_messages(-1001356076506, -1001309160459, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
     
