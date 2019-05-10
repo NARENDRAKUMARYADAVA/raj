7be0c8752b64127eaae24945b2556e34d0cde601
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-10 19:13:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, Filters
 app = Client('765108996:AAGYA2lsT6yw1q5SEx1PXesPWYdwb8RBivc')
-@app.on_message(Filters.chat(-1001309160459) & Filters.text & ~ Filters.regex('👇'))
+@app.on_message(Filters.chat(-1001416872222) & Filters.text & ~ Filters.regex('👇'))
 def forawrd(client, message):
     files = open("sure.txt" , "r")
     file = open("text.txt" , "r")
@@ -10,11 +10,11 @@ def forawrd(client, message):
     files.close()
     for line in lines:
       if line == "started": 
-          client.forward_messages(-1001344956857, -1001309160459, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
-          client.forward_messages(-1001356076506, -1001309160459, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
+          client.forward_messages(-1001344956857, -1001416872222, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
+          client.forward_messages(-1001356076506, -1001416872222, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
     for line in liner:
       if line == "started":
-         client.forward_messages(-1001356076506, -1001309160459, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
+         client.forward_messages(-1001356076506, -1001416872222, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
 @app.on_message(Filters.command('status'))
 def main(client, message) :
   if message.from_user.id == 491634139:
