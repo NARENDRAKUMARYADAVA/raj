4988793c23e8e9a4caedc366b1b0ee31de7a6fff
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-10 15:12:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,5 @@
 from pyrogram import Client, Filters
-
-
 app = Client('765108996:AAGYA2lsT6yw1q5SEx1PXesPWYdwb8RBivc')
-
-
-
-
 @app.on_message(Filters.chat(-1001309160459) & Filters.text & ~ Filters.regex('👇'))
 def forawrd(client, message):
     files = open("sure.txt" , "r")
@@ -14,30 +8,19 @@ def forawrd(client, message):
     liner = files.readlines()
     file.close()
     files.close()
-    
     for line in lines:
       if line == "started": 
           client.forward_messages(-1001344956857, -1001309160459, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
           client.forward_messages(-1001356076506, -1001309160459, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
-    
-    
-    
-
     for line in liner:
       if line == "started":
          client.forward_messages(-1001356076506, -1001309160459, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
-
-
-
-
-
 @app.on_message(Filters.command('status'))
 def main(client, message) :
   if message.from_user.id == 491634139:
       file = open("text.txt" , "r")
       lines = file.readlines()
       file.close()
-
       for line in lines:
         if line == "started":
            message.reply("Forwarding is on ! ")
