4f4e1ea8650fab3b7860a333287179394a399cc8
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-10 13:36:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,21 +8,20 @@ app = Client('765108996:AAGYA2lsT6yw1q5SEx1PXesPWYdwb8RBivc')
 
 @app.on_message(Filters.chat(-1001309160459) & Filters.text & ~ Filters.regex('👇'))
 def forawrd(client, message):
+    files = open("sure.txt" , "r")
     file = open("text.txt" , "r")
     lines = file.readlines()
-    file.close()
-    
-    files = open("sure.txt" , "r")
     liner = files.readlines()
+    file.close()
     files.close()
-
+    
     for line in lines:
       if line == "started": 
           client.forward_messages(-1001344956857, -1001309160459, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
           client.forward_messages(-1001356076506, -1001309160459, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
-    files = open("text.txt" , "r")
-    liner = files.readlines()
-    files.close()
+    
+    
+    
 
     for line in liner:
       if line == "started":
