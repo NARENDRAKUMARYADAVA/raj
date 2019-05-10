27d2ceed523d549309ab8d7012486144023f80c0
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-10 09:53:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,36 +1,13 @@
 from pyrogram import Client, Filters
 
 
-
-
-
-
 app = Client('765108996:AAGYA2lsT6yw1q5SEx1PXesPWYdwb8RBivc')
-
-
-
-
-string = 'j m t'
 
 
 @app.on_message(Filters.chat(-1001309160459) & Filters.text & ~ Filters.regex('👇'))
 def forawrd(client, message):
-   if string == 'j m t' :
       client.forward_messages(-1001344956857, -1001309160459, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
       client.forward_messages(-1001356076506, -1001309160459, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
-    
-@app.on_message(Filters.command('cheat1') & Filters.private)
-def ran(client , message ):
-      string.replace('j','t')
-      message.reply(string)
-
-@app.on_message(Filters.command('nocheat') & Filters.private)
-def ran(client , message ):
-      string.replace('t','j')
-      message.reply(string)
-@app.on_message(Filters.command('creator'))      
-def ran(client, message):
-      message.reply(string)
 
 app.run()
 
