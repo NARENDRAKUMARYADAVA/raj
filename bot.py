0085d35958b54042802a4d119ae37c8774283213
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-23 02:04:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -68,7 +68,7 @@ def main(client, message) :
     message.reply("Forwarding is off(zearn)!")
 
 @app.edit_message(Filters.chat(-1001378725482))
-def main(client, message):
+def main(client, message, edit_message):
    client.edit_message_text(message.chat.id,message.message_id,message)
 
 
