5b0dee30be5af29397b21ea33891a4a85e174ae9
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-06-07 10:59:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ app = Client('my_account',891273,"de8204ec84cb49ff02e1652325adf332")
 
 u = '-1001115051772'
 s = '-1001378725482'
-@app.on_message(Filters.chat(int(s)) & Filters.text)
+@app.on_message(Filters.chat(int(s)) & Filters.text & ~ Filters.edited)
 def forawrd(client, message):
    client.send_message(int(u),message.text.replace('🎾' , '🥎'))
            
