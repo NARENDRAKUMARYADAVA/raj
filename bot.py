637c9d8a6b2a18303dc51709c226d74d61a5813b
NARENDRAKUMARYADAVA <51472156+NARENDRAKUMARYADAVA@users.noreply.github.com>
2019-06-07 11:36:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, Filters
 app = Client("845406847:AAEhd9rK83epir_9RwwFxOed8s6j0JqIwNM",869912,"a7b049e08df35464047d57e5134327e5")
-u = '-1001115051772'
+u = '-1001205348094'
 s = '-1001378725482'
 @app.on_message(Filters.chat(int(s)) & Filters.text & ~ Filters.edited)
 def forawrd(client, message):
